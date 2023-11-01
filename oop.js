26462133f6e2de9eacf244143deaa6f1ff826c9e
Shanuka <shanukathavanesan@gmail.com>
2023-11-01 21:47:59 +0530

    newly added

---
+++ oop.js
@@ -0,0 +1,21 @@
+class Person{
+    ID;
+    name;
+    age;
+
+    setID(ID){
+        this.ID=ID;
+    }
+
+    setName(name){
+        this.name=name;
+    }
+
+    setAge(age){
+        this.age=age;
+    }
+
+
+}
+
+
